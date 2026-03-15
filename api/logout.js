54fd0b5583b4af36321ce71fcba103e95726685c
Cybertrading-Prog <cybertrading@gmx.de>
2026-03-15 07:59:13 +0100

    Delete api directory

--- api/logout.js
+++
@@ -1,3 +0,0 @@
-const { json } = require('../lib/state');
-const { clearSessionCookie } = require('../lib/auth');
-module.exports = async (_req, res) => json(res, 200, { ok: true }, { 'Set-Cookie': clearSessionCookie() });
